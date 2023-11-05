e627236c50ed396dc917087fa5d0ae9166212173
Alan Cruz <llancruzz@alans-mbp.home>
2023-11-05 16:55:30 +0000

    feat: Create addRecipeView

--- src/js/controller.js
+++ src/js/controller.js
@@ -4,6 +4,7 @@ import searchView from "./views/searchView.js";
 import resultsView from "./views/resultsView.js";
 import paginationView from "./views/paginationView.js";
 import bookmarksView from "./views/bookmarksView.js";
+import addRecipeView from "./views/addRecipeView.js";
 
 import "core-js/stable"; // Polyfilling everything
 import "regenerator-runtime/runtime"; // Polyfilling async/await
@@ -96,6 +97,12 @@ const controlBookmarks = function () {
   bookmarksView.render(model.state.bookmarks);
 };
 
+const controlAddRecipe = function (newRecipe) {
+  console.log(newRecipe);
+
+  // Upload the new recipe data
+};
+
 const init = () => {
   bookmarksView.addHandler(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -103,6 +110,7 @@ const init = () => {
   recipeView.addHandlerAddBookmark(controlAddBookmark);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
+  addRecipeView.addHandlerUpload(controlAddRecipe);
 };
 init();
 
